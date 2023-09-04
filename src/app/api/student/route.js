54c8bf4ac20067c5cc53a101682e5a5b101d8546
= <=>
2023-09-04 15:51:10 +0700

    Completed

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -31,6 +31,9 @@ export const GET = async (request) => {
   }
 
   //filter by student id here
+  if (studentId !== null) {
+    filtered = filtered.filter((std) => std.studentId === studentId);
+  }
 
   return NextResponse.json({ ok: true, students: filtered });
 };
@@ -98,18 +101,32 @@ export const PUT = async (request) => {
 
 export const DELETE = async (request) => {
   //get body and validate it
-
+  const body = await request.json();
   //check if student id exist
-
+  const foundIndex = DB.students.findIndex(
+    (std) => std.studentId === body.studentId
+  );
+  if (foundIndex === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Student ID must contain 9 characters`,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
   //perform removing student from DB. You can choose from 2 choices
   //1. use array filter method
   // DB.students = DB.students.filter(...);
-
+  DB.students = DB.students.filter((std) => std.studentId !== body.studentId);
   //or 2. use splice array method
   // DB.students.splice(...)
+  DB.students.splice((std) => std.studentId !== body.studentId);
 
   return NextResponse.json({
     ok: true,
-    message: `Student Id xxx has been deleted`,
+    message: `Student ID ${body.studentId} has been deleted`,
   });
 };
